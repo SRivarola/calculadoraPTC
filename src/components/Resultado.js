269349022f9b386dc1f054fdc51f9cbf174a893a
Santiago Rivarola <srivarola@gmail.com>
2023-06-08 16:58:01 -0300

    semaforo

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -3,8 +3,8 @@ import { resultadoFCS } from "../helpers/textResults"
 const Resultado = ({total}) => {
   return (
     <div className={`w-[100%] md:w-[60%] p-1 font-semibold text-lg rounded-[30px] md:rounded-xl ${total <= 8 && 'bg-[#028D84] text-white'} ${total === 9 && 'bg-[#ffcd5f] text-black'} ${total >= 10 && 'bg-[#a91d2a] text-white'}`}>
-        <p className='text-center'>El resultado es: {total}</p>
-        <p className='text-center' >{resultadoFCS(total)}</p>
+        <p className='text-center tracking-wider'>El resultado es: {total}</p>
+        <p className='text-center tracking-wider' >{resultadoFCS(total)}</p>
     </div>
   )
 }
