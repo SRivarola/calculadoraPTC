28226f33769a2ab0b60de339a8caebf61856a255
Santiago Rivarola <srivarola@gmail.com>
2023-06-08 16:56:50 -0300

    colores semaforo

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -2,7 +2,7 @@ import { resultadoFCS } from "../helpers/textResults"
 
 const Resultado = ({total}) => {
   return (
-    <div className={`w-[100%] md:w-[60%] p-1 font-semibold text-lg rounded-[30px] md:rounded-xl ${total <= 8 && 'bg-green-800 text-white'} ${total === 9 && 'bg-yellow-300 text-[#161616]'} ${total >= 10 && 'bg-red-600 text-white'}`}>
+    <div className={`w-[100%] md:w-[60%] p-1 font-semibold text-lg rounded-[30px] md:rounded-xl ${total <= 8 && 'bg-[#028D84] text-white'} ${total === 9 && 'bg-[#ffcd5f] text-black'} ${total >= 10 && 'bg-[#a91d2a] text-white'}`}>
         <p className='text-center'>El resultado es: {total}</p>
         <p className='text-center' >{resultadoFCS(total)}</p>
     </div>
